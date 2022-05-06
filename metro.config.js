5e4e969951f444065f355a62b3cc540027d0f3f1
volodymyrboichuk <volodymyr.boichuk@logivations.com>
2022-05-06 18:20:32 +0300

    new SDK

--- metro.config.js
+++ metro.config.js
@@ -5,17 +5,12 @@
 
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-	const {
-		resolver: { sourceExts, assetExts },
-	} = await getDefaultConfig(__dirname);
-	return {
-		transformer: {
-			babelTransformerPath: require.resolve('react-native-svg-transformer'),
-		},
-		resolver: {
-			assetExts: assetExts.filter(ext => ext !== 'svg'),
-			sourceExts: [...sourceExts, 'svg', 'mp3'],
-		},
-	};
-})();
+const config = getDefaultConfig(__dirname);
+
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+config.resolver.minifierConfig = {
+	assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
+	sourceExts: [...config.resolver.sourceExts, 'svg', 'mp3'],
+};
+
+module.exports = config;
